be924756f66f13724eed3fce123f89ffc15d1d5f
Shahid Hassan <shaahid.xd@gmail.com>
2018-10-09 21:03:59 +0530

    Altered color change scripts

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
 var fonts = document.getElementsByClassName("font-theme");
 var lines = document.getElementsByClassName("line");
 var navlinks = document.getElementsByClassName("nav-link");
-var isRed = true;
+var background = document.getElementById("background");
+var currentcolor = -1;
+var colorlist = [];
+
+function color(bColor, fColor){
+    this.bColor = bColor;
+    this.fColor = fColor;
+}
 
 function changeTheme(event){
     var noRedirect = '.center-div *';
     if (!event.target.matches(noRedirect)) {
-        var background = document.getElementById("background");
-    
-        if(isRed){
-            background.style.backgroundColor="#FFFFFF";
-            for(var i=0; i< fonts.length; i++){
-                fonts[i].style.color="#8B0000";
-            }
-            for(var i=0; i< lines.length; i++){
-                lines[i].style.borderColor="#8B0000";
-            }
-            for(var i=0; i< navlinks.length; i++){
-                navlinks[i].style.borderColor="#8B0000";
-            }
-            isRed = false;
+        var mrandom = getRandom(colorlist.length-1);
+        while(mrandom === currentcolor){
+            mrandom = getRandom(colorlist.length-1);
         }
-        else{
-            background.style.backgroundColor="#8B0000";
-            for(var i=0; i< fonts.length; i++){
-                fonts[i].style.color="#FFFFFF";
-            }
-            for(var i=0; i< lines.length; i++){
-                lines[i].style.borderColor="#FFFFFF";
-            }
-            for(var i=0; i< navlinks.length; i++){
-                navlinks[i].style.borderColor="#FFFFFF";
-            }
-            isRed = true;
+        background.style.backgroundColor = colorlist[mrandom].bColor;
+        for(var i=0; i< fonts.length; i++){
+            fonts[i].style.color=colorlist[mrandom].fColor;
         }
+        for(var i=0; i< lines.length; i++){
+            lines[i].style.borderColor=colorlist[mrandom].fColor;
+        }
+        for(var i=0; i< navlinks.length; i++){
+            navlinks[i].style.borderColor=colorlist[mrandom].fColor;
+        }
+        currentcolor = mrandom;
     }
 }
 
+function getRandom(max){
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+colorlist.push(new color("#1E1E1E","#FF6600"));
+colorlist.push(new color("#8B0000","#FFFFFF"));
+colorlist.push(new color("#FFA500","#000000"));
+colorlist.push(new color("#FF6600","#1E1E1E"));
+colorlist.push(new color("#FFFFFF","#8B0000"));
+colorlist.push(new color("#000000","#FFA500"));
+
 document.getElementById("background").addEventListener('click',changeTheme);
+//document.getElementById("background").click();
